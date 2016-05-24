264119510632b764d7510ad416c000f8f8f9c601
Federico Ceratto <federico.ceratto@gmail.com>
2016-05-24 10:08:34 +0100

    Add badges

--- nimble_install_test.nim
+++ nimble_install_test.nim
@@ -17,6 +17,9 @@ import times
 const
   nimble_binpath = "./nimble"
   timeout = 60
+  output_dir = "nimble_install_test_output"
+
+let version_badge_tpl = readFile "version-template-blue.svg"
 
 type
   Pkg = tuple[name, url: string]
@@ -24,8 +27,8 @@ type
     OK, FAIL, TIMEOUT
   InstRep* = tuple[title, url: string, test_result: TestResult]
 
-include "install_test_report.tmpl"
-include "install_test_output.tmpl"
+include "nimble_install_test_report.tmpl"
+include "nimble_install_test_output.tmpl"
 
 proc gmtime(): string =
   return $getGmTime(getTime())
@@ -36,22 +39,33 @@ proc load_packages(): seq[Pkg] =
   for pdata in pkg_list:
     result.add((pdata["name"].getStr(), pdata["web"].getStr()))
   result = result.sortedByIt(it[0])
+  # result = result[0..1]
 
-proc write_output(p: Process, pkg: Pkg) =
-  discard p.waitForExit()
-  let output = p.outputStream().readAll()
+proc write_status_badge(output: string, pkg: Pkg) =
+  ## Write stderr/stdout output
   let page = generate_install_report_output_page(pkg.name, output, gmtime())
-  writeFile("nimble_install_output_$#.html" % pkg.name, page)
+  writeFile(output_dir / "$#.html" % pkg.name, page)
+
+proc extract_version(output: string, pkg: Pkg): string =
+  let marker = "Installing $#-" % pkg.name
+  for line in output.splitLines():
+    if line.startsWith(marker):
+      return line[marker.len..^1]
+
+  return "None"
+
+proc write_version_badge(version: string, pkg: Pkg) =
+  let badge = version_badge_tpl % [version, version]
+  writeFile(output_dir / "$#.version.svg" % pkg.name, badge)
 
 proc main() =
+  createDir(output_dir)
 
   let packages = load_packages()
   var installation_reports: seq[InstRep] = @[]
 
   for pkg in packages:
-    let
-      tmp_dir = "/tmp/nimble_install_test/" / pkg.name
-      cmd = "$# install $# --nimbleDir=$# -y" % [$nimble_binpath, $pkg.name, tmp_dir]
+    let tmp_dir = "/tmp/nimble_install_test/" / pkg.name
     createDir(tmp_dir)
     echo "Processing ", $pkg.name
     let
@@ -80,7 +94,16 @@ proc main() =
         TestResult.FAIL
 
     echo $test_result
-    write_output(p, pkg)
+    discard p.waitForExit()
+    let output = p.outputStream().readAll()
+    write_status_badge(output, pkg)
+    if test_result == TestResult.OK:
+      copyFile("success.svg", output_dir / "$#.svg" % pkg.name)
+    else:
+      copyFile("fail.svg", output_dir / "$#.svg" % pkg.name)
+
+    let version = extract_version(output, pkg)
+    write_version_badge(version, pkg)
 
     let r: InstRep = (pkg.name, pkg.url, test_result)
     installation_reports.add r
@@ -91,7 +114,7 @@ proc main() =
   let tstamp = $getGmTime(getTime())
   echo "Writing output"
   let page = generateHTMLPage(installation_reports, tstamp)
-  writeFile("nimble_install_report.html", page)
+  writeFile(output_dir / "nimble_install_report.html", page)
 
 
 when isMainModule:
